e165e87d6c620680bce9145a5365cc597fbf2292
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-06-28 12:46:28 +0000

    [add]ヘッダーのレイアウトを整える(開閉のエラー)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
+
+
