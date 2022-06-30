ea12e072b535865dcb6c8cdd01d47a8d6f28eeeb
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-06-30 21:15:01 +0000

    [add]user-infoの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,20 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @profile_image = User.find(params[:id])
   end
 
   def edit
