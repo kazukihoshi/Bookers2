ba6a7840141dd1003824b7a6a46bd2a870c9cf6b
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-07-14 13:14:38 +0000

    [add]userのshowのメソッドエラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = @user.book
+    @book = @user.books
+    @book_user = current_user
   end
 
   def edit
@@ -13,14 +14,19 @@ class UsersController < ApplicationController
     @user = current_user
     @users = User.all
     @books =Book.all
+    @book = Book.new
   end
   
   def create
     @user = User.new(book_params)
-    @book = Book.new(book_params)
+    @book = @user.book
     @book.user_id = current_user_id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else 
+      render :index
+    end
   end
 
   
